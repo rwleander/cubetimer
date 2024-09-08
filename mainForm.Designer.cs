2787bda62a20a76768a06b156d43c3f7cb7091f7
rwleander <rleander@comcast.net>
2024-09-08 07:47:44 -0600

    initial commit

---
+++ mainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace cubeTimer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(35, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Timer";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(650, 35);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 35);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lvData
+            // 
+            this.lvData.FullRowSelect = true;
+            this.lvData.HideSelection = false;
+            this.lvData.Location = new System.Drawing.Point(35, 100);
+            this.lvData.MultiSelect = false;
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(730, 250);
+            this.lvData.TabIndex = 2;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.lvData);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cube Timer";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView lvData;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,83 @@
+﻿//  cube timer - main form
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cubeTimer
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+                        InitializeComponent();
+        }
+
+        //  variables
+
+        DateTime startTime, endTime; 
+        bool isRunning = false;
+
+
+        //  on load, set up list view
+
+                private void mainForm_Load(object sender, EventArgs e)
+        {
+            lvData.Columns.Clear();
+            lvData.Columns.Add("time", 150, HorizontalAlignment.Center);
+            lvData.Columns.Add("Date", 400, HorizontalAlignment.Left);
+            lvData.Items.Clear();
+        }
+
+        //  on click of start, start the timer
+        //  if timer running, stop the timer and display the message
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (isRunning == true)
+            {
+                endTime = DateTime.Now;
+                TimeSpan diff = endTime- startTime;
+                int mm = diff.Minutes;
+                int ss = diff.Seconds;
+                isRunning = false;
+                MessageBox.Show("Your time is " + mm.ToString() + ":" + ss.ToString());
+                btnStart.Text = "Start Timer";
+                btnClose.Text = "Close";
+            }
+            else
+            {
+                startTime = DateTime.Now;
+                btnStart.Text = "Stop Timer";
+                btnClose.Text = "Cancel";
+                isRunning = true;
+            }                                
+        }
+
+
+        //  close the program
+        //'  if timer is running, cancel the timer
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (isRunning == true)
+            {
+                isRunning = false;
+                btnStart.Text = "Start Timer";
+                btnClose.Text = "Close";
+            }
+            else
+            { 
+            Close();
+        }
+        }
+
+}
+    }
+
